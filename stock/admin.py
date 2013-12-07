408349885e5914791ce32c8cc3a34476e9486bb5
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-12-07 22:59:36 +0100

    Fixed models added admin

--- stock/admin.py
+++ stock/admin.py
@@ -2,4 +2,9 @@ from stock.models import *
 
 from django.contrib import admin
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'quantity')
+    list_filter = ['code']
+    search_field = ['name', 'code']
+    
+admin.site.register(Product, ProductAdmin)
--- stock/models.py
+++ stock/models.py
@@ -5,10 +5,10 @@ from django.utils.translation import ugettext as _
 # Create your models here.
 class Product(models.Model):
     code = models.PositiveIntegerField(_('Code'), primary_key=True)
-    name = models.CharField(_('Product Name'))
+    name = models.CharField(_('Product Name'), max_length=200)
     description = models.CharField(_('Description'), max_length=200)
     quantity = models.PositiveIntegerField(_('Quantity'))
-    producer = models.CharField(_('Description'))
+    producer = models.CharField(_('Producer'), max_length=200)
 
     lastupdate_by = models.ForeignKey('accounts.UserProfile',
                                     related_name='companies_edited',
