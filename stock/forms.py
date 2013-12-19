8327b91891859ef8f2b1ca273877e16bbf393496
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-12-19 22:22:56 +0100

    Added new forms bootstrap based

--- stock/forms.py
+++ stock/forms.py
@@ -1,10 +1,17 @@
-from django import forms
-from django.contrib.admin import widgets
+from django.forms import ModelForm, TextInput
 from django.utils.translation import ugettext as _
 
 from stock.models import Product
 
-class ProductForm(forms.ModelForm):
+class ProductForm(ModelForm):
     class Meta:
         model = Product
         exclude = ('lastupdate', 'lastupdate_by')
+
+        widgets = {
+            'code': TextInput(attrs={'class': 'form-control'}),
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'description': TextInput(attrs={'class': 'form-control'}),
+            'quantity': TextInput(attrs={'class': 'form-control'}),
+            'producer': TextInput(attrs={'class': 'form-control'}),
+        }
