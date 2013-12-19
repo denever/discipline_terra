31ae237d81a398f1d71e1087f33327de94675ef7
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-12-20 00:40:34 +0100

    Fixed widgets types

--- stock/forms.py
+++ stock/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput, NumberInput
 from django.utils.translation import ugettext as _
 
 from stock.models import Product
@@ -10,8 +10,8 @@ class ProductForm(ModelForm):
 
         widgets = {
             'code': TextInput(attrs={'class': 'form-control'}),
-            'name': TextInput(attrs={'class': 'form-control'}),
             'description': TextInput(attrs={'class': 'form-control'}),
-            'quantity': TextInput(attrs={'class': 'form-control'}),
+            'quantity': NumberInput(attrs={'class': 'form-control'}),
+            'wrn_tsh': NumberInput(attrs={'class': 'form-control'}),
             'producer': TextInput(attrs={'class': 'form-control'}),
         }
