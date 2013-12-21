b6ebee7d2c4898811f0c5cbc93553e4b17053aa8
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-12-21 01:09:10 +0100

    Added model Package added barcode to product fixed unicode for product

--- stock/models.py
+++ stock/models.py
@@ -9,17 +9,37 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(_('Quantity'))
     producer = models.CharField(_('Producer'), max_length=200)
     wrn_tsh =  models.PositiveIntegerField(_('Warning Threshold'))
+    barcode = models.PositiveIntegerField(_('Barcode'), max_length=200)
 
     lastupdate_by = models.ForeignKey('accounts.UserProfile',
-                                    related_name='companies_edited',
+                                    related_name='products_edited',
                                     verbose_name=_('Last update by'))
     lastupdate = models.DateTimeField(_('Last update on'), auto_now_add=True)
 
 
     def __unicode__(self):
-        return self.name
+        return self.description
 
     class Meta:
         ordering = ['code']
         verbose_name = _('Product')
         verbose_name_plural = _('Products')
+
+
+class Package(models.Model):
+    product = models.ForeignKey(Product, verbose_name=_('Product'))
+    size = models.PositiveIntegerField(_('Package size'))
+    barcode = models.PositiveIntegerField(_('Barcode'), max_length=200)
+
+    lastupdate_by = models.ForeignKey('accounts.UserProfile',
+                                    related_name='packages_edited',
+                                    verbose_name=_('Last update by'))
+    lastupdate = models.DateTimeField(_('Last update on'), auto_now_add=True)
+
+    def __unicode__(self):
+        return _('Package of %s') % (self.product)
+
+    class Meta:
+        ordering = ['product']
+        verbose_name = _('Package')
+        verbose_name_plural = _('Packages')
