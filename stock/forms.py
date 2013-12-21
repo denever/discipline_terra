17e2e56d91a6573a26bff11a0122d00ab1434c11
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-12-21 01:09:28 +0100

    Added PackageForm

--- stock/forms.py
+++ stock/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm, TextInput, NumberInput
 from django.utils.translation import ugettext as _
 
-from stock.models import Product
+from stock.models import Product, Package
 
 class ProductForm(ModelForm):
     class Meta:
@@ -14,4 +14,13 @@ class ProductForm(ModelForm):
             'quantity': NumberInput(attrs={'class': 'form-control'}),
             'wrn_tsh': NumberInput(attrs={'class': 'form-control'}),
             'producer': TextInput(attrs={'class': 'form-control'}),
+        }
+
+class PackageForm(ModelForm):
+    class Meta:
+        model = Package
+        exclude = ('lastupdate', 'lastupdate_by')
+        widgets = {
+            'size': NumberInput(attrs={'class': 'form-control'}),
+            'barcode': NumberInput(attrs={'class': 'form-control'}),
         }
