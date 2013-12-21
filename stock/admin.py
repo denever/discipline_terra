dc76d75d5b23307aa587efbb91dca968da894dce
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-12-21 01:08:11 +0100

    Added admin model for Package

--- stock/admin.py
+++ stock/admin.py
@@ -5,6 +5,13 @@ from django.contrib import admin
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('code', 'description', 'quantity')
     list_filter = ['code']
-    search_field = ['description', 'code']
+    search_field = ['description', 'code', 'barcode']
     
 admin.site.register(Product, ProductAdmin)
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'barcode', 'size')
+    list_filter = ['product']
+    search_field = ['product', 'barcode']
+
+admin.site.register(Package, PackageAdmin)
