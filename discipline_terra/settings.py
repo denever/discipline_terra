f77d6b48e0217fa6105cf5326429b95c773cebb7
Giuseppe Martino <martinogiuseppe@gmail.com>
2014-07-14 12:25:45 +0200

    Fixed directory using current working dir

--- discipline_terra/settings.py
+++ discipline_terra/settings.py
@@ -5,6 +5,8 @@ import os.path
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+CURRENT_DIR = os.getcwd()
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -14,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(os.getcwd(), 'discipline.db'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(CURRENT_DIR, 'discipline.db'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -74,7 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/denever/work/discipline_terra/discipline_terra/static',
+    os.path.join(CURRENT_DIR,'discipline_terra/static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -114,7 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/denever/work/discipline_terra/discipline_terra/'
+    os.path.join(CURRENT_DIR,'discipline_terra')
 )
 
 INSTALLED_APPS = (
