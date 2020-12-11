87eb7a6415e0f88d02ba0698108071ed49935b6c
Giuseppe Martino <martinogiuseppe@gmail.com>
2020-12-11 10:53:33 +0100

    Hide secret key

--- discipline_terra/settings.py
+++ discipline_terra/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "=)p)t-f-4s9%6!yf)uc1k(0y-^96)()kwjjyxjiy&v6soxrc3w"
+SECRET_KEY = "FAKE_SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- local_settings.py
+++ local_settings.py
@@ -1,33 +1,36 @@
-import sys
 import os.path
+import sys
 
 CURRENT_DIR = os.getcwd()
 
-############### Local settings #################
+# Local settings
+# Set engine to django.db.backends. 'postgresql_psycopg2', 'mysql', 'sqlite3'
+# Or path to database file if using sqlite3.
+# Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
 
-LOCALE_PATHS = ('/home/denever/work/discipline_terra/conf/locale',)
+LOCALE_PATHS = ("/home/denever/work/discipline_terra/conf/locale",)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(CURRENT_DIR, 'discipline.db'),                      # Or path to database file if using sqlite3.
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(CURRENT_DIR, "discipline.db"),
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",  # Set to empty string for default.
     }
 }
-DEBUG = False if 'test' in sys.argv else True
+DEBUG = False if "test" in sys.argv else True
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(CURRENT_DIR,'discipline_terra/static'),
+    os.path.join(CURRENT_DIR, "discipline_terra/static"),
 )
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(CURRENT_DIR,'discipline_terra')
+    os.path.join(CURRENT_DIR, "discipline_terra")
 )
